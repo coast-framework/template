765e9c6ef8cd6469ebd3a73a23cc2786d0ea2949
Sean Walker <swlkr@fastmail.com>
2018-09-26 15:44:46 -0700

    Update template

--- src/server.clj
+++ src/server.clj
@@ -1,9 +1,11 @@
 (ns server
   (:require [coast]
-            [routes :refer [routes]])
+            [routes]
+            [components])
   (:gen-class))
 
-(def app (coast/app {:routes routes}))
+(def app (coast/app {:routes routes/routes
+                     :layout components/layout}))
 
 (defn -main [& [port]]
   (coast/server app {:port port}))

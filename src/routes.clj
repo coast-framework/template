5098662dba7519a5ba4ac2901c8bc76df819cedc
Sean Walker <swlkr.rbl@gmail.com>
2019-03-16 10:32:40 -0600

    Fix site-routes components reference

--- src/routes.clj
+++ src/routes.clj
@@ -1,10 +1,9 @@
 (ns routes
-  (:require [coast]
-            [components]))
+  (:require [coast]))
 
 (def routes
   (coast/routes
-    (coast/site-routes components/layout
+    (coast/site-routes :components/layout
       [:get "/" :home/index]
       [:404 :home/not-found]
       [:500 :home/server-error])))

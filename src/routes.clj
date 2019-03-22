35521d1ea0b6e9659481b80d1989cc42b61cb8fa
Sean Walker <swlkr.rbl@gmail.com>
2019-03-21 23:35:05 -0600

    Use newer function names

--- src/routes.clj
+++ src/routes.clj
@@ -2,8 +2,9 @@
   (:require [coast]))
 
 (def routes
-  (coast/routes
-    (coast/site-routes :components/layout
-      [:get "/" :home/index]
-      [:404 :home/not-found]
-      [:500 :home/server-error])))
+  (coast/site
+    (coast/with-layout :components/layout
+      [:get "/" :home/index])
+
+    [:404 :home/not-found]
+    [:500 :home/server-error]))

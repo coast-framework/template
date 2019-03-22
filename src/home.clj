4d38811686d9bbc1639fc09be8e5221bb035e429
Sean Walker <swlkr.rbl@gmail.com>
2019-03-21 23:34:54 -0600

    Don't return maps from 404/500 handlers

--- src/home.clj
+++ src/home.clj
@@ -7,22 +7,20 @@
 
 
 (defn not-found [request]
-  (coast/not-found
-    [:html
-     [:head
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-      (coast/css "bundle.css")
-      (coast/js "bundle.js")]
-     [:body
-      [:h1 "Couldn't find what you were looking for"]]]))
+  [:html
+   [:head
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+    (coast/css "bundle.css")
+    (coast/js "bundle.js")]
+   [:body
+    [:h1 "Couldn't find what you were looking for"]]])
 
 
 (defn server-error [request]
-  (coast/server-error
-    [:html
-      [:head
-       [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-       (coast/css "bundle.css")
-       (coast/js "bundle.js")]
-      [:body
-       [:h1 "Something went wrong!"]]]))
+  [:html
+    [:head
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+     (coast/css "bundle.css")
+     (coast/js "bundle.js")]
+    [:body
+     [:h1 "Something went wrong!"]]])

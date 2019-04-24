048d974b40ee3ba25a0ecd984bc601a9ee8c5e3e
Sean Walker <swlkr.rbl@gmail.com>
2019-04-24 09:54:25 -0600

    Add 404/500 html files

--- src/site/home.clj
+++ src/site/home.clj
@@ -5,23 +5,3 @@
 (defn index [request]
   [:h1 {:class "tc"}
    "You're coasting on clojure!"])
-
-
-(defn not-found [request]
-  [:html
-   [:head
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-    (coast/css "bundle.css")
-    (coast/js "bundle.js")]
-   [:body
-    [:h1 "Couldn't find what you were looking for"]]])
-
-
-(defn server-error [request]
-  [:html
-    [:head
-     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-     (coast/css "bundle.css")
-     (coast/js "bundle.js")]
-    [:body
-     [:h1 "Something went wrong!"]]])

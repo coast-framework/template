6f13935c8d433c0f1e09c701c56b9955e066896f
Sean Walker <swlkr.rbl@gmail.com>
2019-07-10 00:06:28 -0600

    Move layout to it's own file

---
+++ src/layout.clj
@@ -0,0 +1,17 @@
+(ns layout
+  (:require [coast]))
+
+
+(defn app [request response]
+  (let [{:keys [body title]} response]
+    (coast/render :html
+      (coast/html
+        (coast/doctype :html5)
+        [:html {:lang "en"}
+         [:head
+          [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+          [:title (or title "Coast on Clojure")]
+          (coast/css "bundle.css")]
+         [:body
+          body
+          (coast/js "bundle.js")]]))))

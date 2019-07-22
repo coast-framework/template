1953e9ed6d293c7f1f6340bb1ec7779589a38f2c
Sean Walker <swlkr.rbl@gmail.com>
2019-07-22 15:49:46 -0600

    More reusable components

--- src/components.clj
+++ src/components.clj
@@ -2,38 +2,14 @@
   (:require [coast]))
 
 
-(defn layout [request body]
-  [:html
-    [:head
-     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-     (coast/css "bundle.css")
-     (coast/js "bundle.js")]
-    [:body
-     body]])
-
-
-(defn link-to [url & body]
-  [:a {:href url :class "f6 link underline blue"}
-    body])
-
-
-(defn button-to
-  ([am m s]
-   (let [data (select-keys m [:data-confirm])
-         form (select-keys am [:action :_method :method :class])]
-     (coast/form (merge {:class "dib ma0"} form)
-       [:input (merge data {:class "input-reset pointer link underline bn f6 br2 ma0 pa0 dib blue bg-transparent"
-                            :type "submit"
-                            :value s})])))
-  ([am s]
-   (button-to am {} s)))
-
-
-(defn container [m & body]
-  (let [mw (or (:mw m) 8)]
-    [:div {:class (str "pa4 w-100 center mw" mw)}
-     [:div {:class "overflow-auto"}
-       body]]))
+(defn container
+  ([m body]
+   (let [class (get m :class)
+         attrs (dissoc m :class)]
+     [:div (merge attrs {:class (str "pa4 center " class)})
+      body]))
+  ([body]
+   (container {:class "mw8"} body)))
 
 
 (defn table [& body]
@@ -65,12 +41,6 @@
   [:td {:class "pa3"} body])
 
 
-(defn submit [value]
-  [:input {:class "input-reset pointer dim ml3 db bn f6 br2 ph3 pv2 dib white bg-blue"
-           :type "submit"
-           :value value}])
-
-
 (defn dt [s]
   [:dt {:class "f6 b mt2"} s])
 
@@ -83,40 +53,58 @@
   [:dl body])
 
 
-(defn form-for
-  ([k body]
-   (form-for k {} body))
-  ([k m body]
-   (form-for k m {} body))
-  ([k m params body]
-   (coast/form-for k m (merge params {:class "pa4"})
-     [:div {:class "measure"}
-      body])))
-
-
 (defn label [m s]
   [:label (merge {:for s :class "f6 b db mb2"} m) s])
 
 
-(defn input [m]
-  [:input (merge {:class "input-reset ba b--black-20 pa2 mb2 db w-100 outline-0"} m)])
+(defn vstack [m & body]
+  [:vstack m body])
 
 
-(defn text-muted [s]
-  [:div {:class "f6 tc gray"}
-   s])
+(defn hstack [m & body]
+  [:hstack m body])
 
 
-(defn el [k m]
-  (fn [& body]
-    [k m body]))
+(defn form-for [v & body]
+  (coast/form (apply coast/action-for v)
+    body))
 
 
-(->> [:mr1 :mr2 :mr3 :mr4 :mr5]
-     (mapv name)
-     (mapv #(coast/intern-var % (el :span {:class %}))))
+(defn field [type table column & {:as options}]
+  (let [table (name table)
+        column (name column)]
+    [:input (assoc options
+              :type type
+              :name (str table "[" column "]"))]))
 
 
-(defn tc [& body]
-  [:div {:class "tc"}
-   body])
+(def text-field (partial field "text"))
+(def email-field (partial field "email"))
+(def password-field (partial field "password"))
+(def number-field (partial field "number"))
+(def tel-field (partial field "tel"))
+
+
+(defn field-error [error]
+  [:span {:class "f6"}
+   error])
+
+
+(defn submit [value & {:as options}]
+  [:input (merge {:type "submit" :value value} options)])
+
+
+(defn link-to
+  ([url-vector m body]
+   [:a (merge {:href (apply coast/url-for url-vector)} m)
+     body])
+  ([url-vector body]
+   (link-to url-vector {} body)))
+
+
+(defn button-to
+  ([action-vector m body]
+   (coast/form (merge (apply coast/action-for action-vector) {:class "dib ma0"})
+     [:button m body]))
+  ([action-vector body]
+   (button-to action-vector {} body)))

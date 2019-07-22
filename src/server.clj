03d2f37c75cbb04e421470195e798c37ccd4b87c
Sean Walker <swlkr.rbl@gmail.com>
2019-07-22 15:49:21 -0600

    Use new exposed middleware

--- src/server.clj
+++ src/server.clj
@@ -1,12 +1,30 @@
 (ns server
   (:require [coast]
-            [routes])
-  (:gen-class))
+            [layout]
+            [routes]))
 
-(def app (coast/app {:routes routes/routes}))
+
+(def db (coast/db))
+
+
+(def app (-> (coast/app routes/app)
+             (coast/layout layout/app)
+             (coast/set-db db)
+             (coast/not-found)
+             (coast/server-error)
+             (coast/sessions)
+             (coast/body-parser)
+             (coast/cookies)
+             (coast/head)
+             (coast/security-headers)
+             (coast/logger)
+             (coast/assets)
+             (coast/reload)))
+
 
 (defn -main [& [port]]
   (coast/server app {:port port}))
+
 
 (comment
   (-main))

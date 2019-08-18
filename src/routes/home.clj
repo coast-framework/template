43bf6f0eea1110a0df410f46f4c2e07d994acbe8
Sean Walker <swlkr.rbl@gmail.com>
2019-08-17 19:43:09 -0600

    Use actual fn on routes, not keyword

--- src/routes/home.clj
+++ src/routes/home.clj
@@ -2,11 +2,11 @@
   (:require [coast]))
 
 
-(def routes
-  (coast/routes
-    [:get "/" ::index]))
-
-
 (defn index [request]
   [:h1 {:class "tc"}
     "You're coasting on clojure!"])
+
+
+(def routes
+  (coast/routes
+    [:get "/" index]))
